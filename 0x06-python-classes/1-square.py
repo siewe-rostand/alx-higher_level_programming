94c6fbf04c1ecf8f55cf88bd03000133a6428639
siewe-rostand <rostand.siewe9@gmail.com>
2023-10-24 18:59:22 -0700

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,9 +2,14 @@
 # 1-square
 """ Defines a square """ 
 class Square:
-    """ Represents a square """
+    """ Represents a square
+    Args:
+        size (int): represents the size of the square defined
+    """
 
     def __init__(self, size):
         """Initializing this square class
-        Args: size - represents the size of the square defined"""
+        Args: size - represents the size of the square defined
+        """
+
         self.__size = size
