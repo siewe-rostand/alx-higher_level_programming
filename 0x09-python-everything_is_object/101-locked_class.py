3884fa1d10ef47bb59552ec7dda8dfdee474e195
siewe-rostand <rostand.siewe9@gmail.com>
2023-10-31 15:35:17 -0700

    30. Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,8 +8,16 @@ dynmaically created attributes
 
 
 class LockedClass:
+    """LockedClass prevents dynamic creation of attributes other than
+    `first_name`
+    """
     __slots__ = ['first_name']
 
-    def __init__(self):
-        """ Init method """
-        pass
+
+if __name__ == '__main__':
+    lc = LockedClass()
+    lc.first_name = "John"
+    try:
+        lc.last_name = "Snow"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
